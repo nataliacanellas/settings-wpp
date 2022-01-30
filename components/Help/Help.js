0f74426f8ffde43adf34af403b03a114a9ef4e9f
Natalia F. Canellas <nataliafcanellas@gmail.com>
2022-01-30 05:21:16 -0300

    Created Help Component

---
+++ components/Help/Help.js
@@ -0,0 +1,29 @@
+import React from 'react';
+
+import { 
+  Container, 
+  Box, 
+  BoxLeft,
+  ViewIcon,
+  PrincipalIcon, 
+  BoxName, 
+  ArrowIcon,
+} from './styles';
+
+const Help = () => {
+  return (
+    <Container>
+      <Box>
+        <BoxLeft>
+          <ViewIcon>
+            <PrincipalIcon/>
+          </ViewIcon>
+          <BoxName>Help</BoxName>
+        </BoxLeft>
+        <ArrowIcon/>
+      </Box>
+    </Container>
+  );
+}
+
+export default Help;
