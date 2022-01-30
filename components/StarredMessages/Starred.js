6c3f72ed9fc8a62004214ce7653d604d893595a6
Natalia F. Canellas <nataliafcanellas@gmail.com>
2022-01-30 04:04:40 -0300

    ✨ Created Starred Messages component

---
+++ components/StarredMessages/Starred.js
@@ -0,0 +1,29 @@
+import React from 'react';
+
+import { 
+  Container, 
+  Box, 
+  BoxLeft,
+  ViewIcon,
+  PrincipalIcon, 
+  BoxName, 
+  ArrowIcon,
+} from './styles';
+
+const StarredMessages = () => {
+  return (
+    <Container>
+      <Box>
+        <BoxLeft>
+          <ViewIcon>
+            <PrincipalIcon/>
+          </ViewIcon>
+          <BoxName>Starred Messages</BoxName>
+        </BoxLeft>
+        <ArrowIcon/>
+      </Box>
+    </Container>
+  );
+}
+
+export default StarredMessages;
