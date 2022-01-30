1f04c6201fb8d9ee81a61a7b38832aae8d50e3c9
Natalia F. Canellas <nataliafcanellas@gmail.com>
2022-01-30 05:20:00 -0300

    ✨ Created notification component

---
+++ components/Notifications/Notifications.js
@@ -0,0 +1,29 @@
+import React from 'react';
+
+import { 
+  Container, 
+  Box, 
+  BoxLeft,
+  ViewIcon,
+  PrincipalIcon, 
+  BoxName, 
+  ArrowIcon,
+} from './styles';
+
+const Notifications = () => {
+  return (
+    <Container>
+      <Box>
+        <BoxLeft>
+          <ViewIcon>
+            <PrincipalIcon/>
+          </ViewIcon>
+          <BoxName>Notifications</BoxName>
+        </BoxLeft>
+        <ArrowIcon/>
+      </Box>
+    </Container>
+  );
+}
+
+export default Notifications;
